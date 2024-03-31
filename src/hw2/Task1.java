1771738125ae36bc4b368d203f6ebab8addedd2e
Katarina199710 <kitty199710@gmail.com>
2024-03-31 22:28:48 +0300

    hw2 task1

--- src/hw2/Task1.java
+++ src/hw2/Task1.java
@@ -5,7 +5,7 @@ public class Task1 {
        Создать строку string2 в которой должно быть помещено значение строки string1,
        Обрезанное до "This line that i want to cut, cause".
        Создать строку string3 на основе string2 которое будет содержать значение
-       "This line that don't want to cut, cause it is perfect".
+       "This line that i don't want to cut, cause it is perfect".
        Вывести на консоль каждое сообщение и его длину.*/
 
     public static void main(String[] args) {
@@ -18,6 +18,7 @@ public class Task1 {
         System.out.println(string2.length());
         String string3;
         string3 = string2.substring(0,string2.length()-20).concat("don't want to cut, cause it is perfect");
+//        string3 = string2.substring(0,17).concat("don't want to cut, cause it is perfect");
         System.out.println(string3);
         System.out.println(string3.length());
 
