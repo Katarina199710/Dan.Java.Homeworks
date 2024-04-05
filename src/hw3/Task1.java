2ad41f6e869ab00a5963530f9febe58d3b1f2733
Katarina199710 <kitty199710@gmail.com>
2024-04-05 20:33:30 +0300

    modified task 1 hw3

--- src/hw3/Task1.java
+++ src/hw3/Task1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Task1 {
     /* У вас есть строка "Я тестирую замечательно. Что еще нужно?", которую нужно ввести с клавиатуры в консоль.
-   Необходимо при помощи только  метода next() класса Scanner (не используем метод nextLine()) присвоить переменным
+   Необходимо при помощи только метода next() класса Scanner (не используем метод nextLine()) присвоить переменным
    типа String следующие значения:
    string1 = Я
            string2 = тестирую
@@ -14,15 +14,13 @@ public class Task1 {
    создавать нельзя.*/
     public static void main(String[] args) {
         Scanner scannerForHW3 = new Scanner(System.in);
-        String string1 = "Я";
-        String string2 = "тестирую";
-        String string3 = "замечательно.";
-        String string4 = "Что еще нужно?";
         System.out.println("Insert text:");
-        System.out.println(scannerForHW3.next());
-        System.out.println(scannerForHW3.next());
-        System.out.println(scannerForHW3.next());
-        System.out.println(scannerForHW3.next() + string4.replace("Что",""));
+        String string1 = scannerForHW3.next();
+        String string2 = scannerForHW3.next();
+        String string3 = scannerForHW3.next();
+        String string4 = scannerForHW3.next();
+        System.out.println(string1 + " " + string2 + " " + string3 + " " + string4);
         scannerForHW3.close();
     }
 }
+// Тут не поняла как сделать чтобы оно не обрезало последнюю фразу, которую мы вводим
