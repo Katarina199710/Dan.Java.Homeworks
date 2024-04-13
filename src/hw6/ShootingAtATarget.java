3c19f2157541f2fcc662a47d38c779d24de59d6e
Katarina199710 <kitty199710@gmail.com>
2024-04-13 14:17:59 +0300

    hw6 final

--- src/hw6/ShootingAtATarget.java
+++ src/hw6/ShootingAtATarget.java
@@ -14,42 +14,45 @@ public class ShootingAtATarget {
         int random2 = random.nextInt(1, 5);
         Scanner scanner = new Scanner(System.in);
         System.out.println("All Set. Get ready to rumble!");
-        System.out.println("Enter your line number from 1 to 5");
-        int number1 = scanner.nextInt();
-        System.out.println("Enter your column number from 1 to 5");
-        int number2 = scanner.nextInt();
-//        int number1 = 0;
-//        int number2 = 0;
-        while (random1!=number1 && random2!=number2) {
-//            int number1 = scanner.nextInt();
-            if (number1 >= 1 && number1 <= 5) {
-                System.out.println("Enter your column number from 1 to 5");
-                number2 = scanner.nextInt();
-            } else if (number2 >= 1 && number2 <= 5){
-                System.out.println("Enter your number again");
-                number1 = scanner.nextInt();
-            }
-            if (number2 >= 1 && number2 <= 5) {
-                for (int i = 0; i < field.length; i++) {
-                    System.out.print("[");
-                    for (int a = 0; a < field[i].length; a++) {
-                        System.out.print(field[i][a] + ", ");
-                    }
-                    System.out.print("\b] ");
-                    System.out.println();
+
+        boolean c = true;
+        while (c) {
+            boolean b = true;
+            System.out.println("Enter your line number from 1 to 5");
+            int number1 = scanner.nextInt();
+            while (b) {
+                if (number1 >= 1 && number1 <= 5) {
+                    b = false;
+                } else {
+                    System.out.println("Enter your number again");
+                    number1 = scanner.nextInt();
                 }
-            } else {
-                System.out.println("Enter your number again");
-                number2 = scanner.nextInt();
             }
-//            for (int i = 0; i < field.length; i++) {
-//                System.out.print("[");
-//                for (int a = 0; a < field[i].length; a++) {
-//                    System.out.print(field[i][a] + ", ");
-//                }
-//                System.out.print("\b] ");
-//                System.out.println();
-//            }
+            System.out.println("Enter your column number from 1 to 5");
+            int number2 = scanner.nextInt();
+            boolean b1 = true;
+            while (b1) {
+                if (number2 >= 1 && number2 <= 5) {
+                    b1 = false;
+                } else {
+                    System.out.println("Enter your number again");
+                    number2 = scanner.nextInt();
+                }
+            }
+            if (random1 == number1 && random2 == number2) {
+                field[number1][number2] = 'x';
+                c = false;
+            } else {
+                field[number1][number2] = '*';
+            }
+            for (int i = 0; i < field.length; i++) {
+                System.out.print("[");
+                for (int a = 0; a < field[i].length; a++) {
+                    System.out.print(field[i][a] + ", ");
+                }
+                System.out.print("\b] ");
+                System.out.println();
+            }
         }
     }
 }
