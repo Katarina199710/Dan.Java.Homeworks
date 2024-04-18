b790665010111eaaa8b4717c8fdd0f1e74ea2147
Katarina199710 <kitty199710@gmail.com>
2024-04-18 15:16:29 +0300

    hw8 modified converter

--- src/hw8/task3/Converter.java
+++ src/hw8/task3/Converter.java
@@ -11,111 +11,111 @@ public class Converter {
         return name;
     }
 
-    public void convertToInt(byte a) {
-        int a1 = Integer.parseInt(String.valueOf(a));
+    public int convertToInt(byte a) {
+        return Integer.parseInt(String.valueOf(a));
     }
 
-    public void convertToInt(short a) {
-        int a1 = Integer.parseInt(String.valueOf(a));
+    public int convertToInt(short a) {
+        return Integer.parseInt(String.valueOf(a));
     }
 
-    public void convertToInt(int a) {
-        int a1 = Integer.parseInt(String.valueOf(a));
+    public int convertToInt(int a) {
+        return Integer.parseInt(String.valueOf(a));
     }
 
-    public void convertToInt(long a) {
-        int a1 = Integer.parseInt(String.valueOf(a));
+    public int convertToInt(long a) {
+        return Integer.parseInt(String.valueOf(a));
     }
 
-    public void convertToInt(char a) {
-        int a1 = Integer.parseInt(String.valueOf(a));
+    public int convertToInt(char a) {
+        return Integer.parseInt(String.valueOf(a));
     }
 
-    public void convertToInt(float a) {
-        int a1 = Integer.parseInt(String.valueOf(a));
+    public int convertToInt(float a) {
+        return Integer.parseInt(String.valueOf(a));
     }
 
-    public void convertToInt(double a) {
-        int a1 = Integer.parseInt(String.valueOf(a));
+    public int convertToInt(double a) {
+        return Integer.parseInt(String.valueOf(a));
     }
 
-    public void convertToInt(String a) {
-        int a1 = Integer.parseInt(a);
+    public int convertToInt(String a) {
+        return Integer.parseInt(a);
     }
 
     public void convertToInt(boolean a) {
         System.out.println("You inserted boolean");
     }
 
-    public void convertToDouble(byte a) {
-        double a1 = Double.parseDouble(String.valueOf(a));
+    public double convertToDouble(byte a) {
+        return Double.parseDouble(String.valueOf(a));
     }
 
-    public void convertToDouble(short a) {
-        double a1 = Double.parseDouble(String.valueOf(a));
+    public double convertToDouble(short a) {
+        return Double.parseDouble(String.valueOf(a));
     }
 
-    public void convertToDouble(int a) {
-        double a1 = Double.parseDouble(String.valueOf(a));
+    public double convertToDouble(int a) {
+        return Double.parseDouble(String.valueOf(a));
     }
 
-    public void convertToDouble(long a) {
-        double a1 = Double.parseDouble(String.valueOf(a));
+    public double convertToDouble(long a) {
+        return Double.parseDouble(String.valueOf(a));
     }
 
-    public void convertToDouble(char a) {
-        double a1 = Double.parseDouble(String.valueOf(a));
+    public double convertToDouble(char a) {
+        return Double.parseDouble(String.valueOf(a));
     }
 
-    public void convertToDouble(float a) {
-        double a1 = Double.parseDouble(String.valueOf(a));
+    public double convertToDouble(float a) {
+        return Double.parseDouble(String.valueOf(a));
     }
 
-    public void convertToDouble(double a) {
-        double a1 = Double.parseDouble(String.valueOf(a));
+    public double convertToDouble(double a) {
+        return Double.parseDouble(String.valueOf(a));
     }
 
-    public void convertToDouble(String a) {
-        double a1 = Double.parseDouble(a);
+    public double convertToDouble(String a) {
+        return Double.parseDouble(a);
     }
 
     public void convertToDouble(boolean num) {
         System.out.println("You inserted boolean");
     }
 
-    public void convertToString(byte a) {
-        String a1 = String.valueOf(a);
+    public String convertToString(byte a) {
+        return String.valueOf(a);
     }
 
-    public void convertToString(short a) {
-        String a1 = String.valueOf(a);
+    public String convertToString(short a) {
+        return String.valueOf(a);
     }
 
-    public void convertToString(int a) {
-        String a1 = String.valueOf(a);
+    public String convertToString(int a) {
+        return String.valueOf(a);
     }
 
-    public void convertToString(long a) {
-        String a1 = String.valueOf(a);
+    public String convertToString(long a) {
+        return String.valueOf(a);
     }
 
-    public void convertToString(char a) {
-        String a1 = String.valueOf(a);
+    public String convertToString(char a) {
+        return String.valueOf(a);
     }
 
-    public void convertToString(float a) {
-        String a1 = String.valueOf(a);
+    public String convertToString(float a) {
+        return String.valueOf(a);
     }
 
-    public void convertToString(boolean a) {
-        String a1 = String.valueOf(a);
+    public String convertToString(boolean a) {
+        return String.valueOf(a);
     }
 
-    public void convertToString(double a) {
-        String a1 = String.valueOf(a);
+    public String convertToString(double a) {
+        return String.valueOf(a);
     }
 
-    public void convertToString(String a) {
-        String a1 = String.valueOf(a);
+    public String convertToString(String a) {
+        return String.valueOf(a);
     }
 }
